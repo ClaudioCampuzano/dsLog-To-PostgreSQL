f366a88ad6a8f9f3a281071eb97b80a722d48c83
Claudio Campuzano <claudio.campuzano.b@gmail.com>
2021-12-23 13:07:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def recordExtractor(mallId, file, analitica):
         for rec in reversed(recordFromFile):
             for source in range(sourceNumber):
                 if rec[-1] > lastTime[source]:
-                    recordInsert.append((mallId, source,rec[source], rec[-1].strftime("%d-%m-%Y"), rec[-1].strftime("%:-%M:%S"), analitica))
+                    recordInsert.append((mallId, source,rec[source], rec[-1].strftime("%d-%m-%Y"), rec[-1].strftime("%H:%M:%S"), analitica))
                 
         print("Finish,", len(recordInsert), "records")
         
